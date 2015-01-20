5bba7a16cd11ce641c2466db445789aa54c6e785
TADA Tadashi <t@tdtds.jp>
2015-01-20 14:11:18 +0900

    hide calendar in conf modes and replace feed icon to local file

--- misc/plugin/navi_with_calendar.rb
+++ misc/plugin/navi_with_calendar.rb
@@ -7,8 +7,10 @@ def navi
 	result = %Q[<div class="adminmenu">\n]
 	result << navi_user
 	result << navi_admin
-	result << calendar
-	result << %Q[<span class="adminmenu"><a href="index.rdf"><img style="border-width: 0px;" src="http://tdiary1.tdiary.net/feed-icon-12x12.png" width="12" height="12" alt="RSS feed"></a></span>]
+	unless @mode =~ /conf/
+		result << calendar
+		result << %Q[<span class="adminmenu"><a href="index.rdf"><img style="border-width: 0px;" src="/images/feed-icon-12x12.png" width="12" height="12" alt="RSS feed"></a></span>]
+	end
 	result << %Q[</div>]
 end
 
