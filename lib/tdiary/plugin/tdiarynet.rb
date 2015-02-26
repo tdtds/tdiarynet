7ec2a951217ca0a528d6708550ca1a749e726ae0
TADA Tadashi <t@tdtds.jp>
2015-02-26 18:35:06 +0900

    insert warning messages to suspened diary

---
+++ lib/tdiary/plugin/tdiarynet.rb
@@ -0,0 +1,13 @@
+#
+# default plugins for tDiary.Net
+#
+
+add_footer_proc do
+	footer = '<div class="footer">This Diary is Running on <a href="http://www.tdiary.net/">tDiary.Net</a> 1st server. Hosted by <a href="http://www.interlink.or.jp/service/">インターリンク</a>&nbsp;<a href="http://hosting.interlink.or.jp/">ホスティング</a></div>'
+
+	if @conf['tdiarynet_suspended']
+		footer << %Q|<div style="position:absolute;top:0px;left:0px;color:#000;background-color:#fff;padding:32px;">この日記は更新が止まっているので変更不可になっています。再度更新したい場合には<a href="http://www.tdiary.net/bbs/support.html">掲示板<a>にてその旨をお知らせ下さい。折返し管理者より連絡します。</div>|
+	end
+
+	footer
+end
