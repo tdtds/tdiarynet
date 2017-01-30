d71044ab7bd5394d4afa0aa3d261578a27ae1b3c
TADA Tadashi <t@tdtds.jp>
2017-01-30 11:45:38 +0900

    change google URL to "https"

--- misc/plugin/search-google.rb
+++ misc/plugin/search-google.rb
@@ -3,7 +3,7 @@
 #
 def search
 	result = <<-TEXT
-		<form method="get" action="http://www.google.co.jp/search" class="search"><div>
+		<form method="get" action="https://www.google.co.jp/search" class="search"><div>
 		<input type="hidden" name="as_sitesearch" value="#{@conf.user_name}.tdiary.net">
 		<input class="text" type="text" name="as_q" value="#{show_keyword}">
 		<input class="button" type="submit" value="Go">
