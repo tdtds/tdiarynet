68c5b6529079753ed0c026b53978024301d3624d
Tada, Tadashi <t@tdtds.jp>
2020-03-11 13:37:28 +0900

    replace footer url to https
    Signed-off-by: Tada, Tadashi <t@tdtds.jp>

--- lib/tdiary/plugin/tdiarynet.rb
+++ lib/tdiary/plugin/tdiarynet.rb
@@ -2,10 +2,10 @@
 # default plugins for tDiary.Net
 #
 add_footer_proc do
-	footer = '<div class="footer">This Diary is Running on <a href="http://www.tdiary.net/">tDiary.Net</a> 1st server. Hosted by <a href="http://www.interlink.or.jp/service/">インターリンク</a>&nbsp;<a href="http://hosting.interlink.or.jp/">ホスティング</a></div>'
+	footer = '<div class="footer">This Diary is Running on <a href="https://www.tdiary.net/">tDiary.Net</a> 1st server. Hosted by <a href="https://www.interlink.or.jp/service/">インターリンク</a>&nbsp;<a href="https://hosting.interlink.or.jp/">ホスティング</a></div>'
 
 	if @conf['tdiarynet_suspended']
-		footer << %Q|<div style="position:absolute;top:0px;left:0px;color:#000;background-color:#fff;padding:32px;">この日記は更新が止まっているので変更不可になっています。再度更新したい場合には<a href="http://www.tdiary.net/bbs/support.html">掲示板<a>にてその旨をお知らせ下さい。折返し管理者より連絡します。</div>|
+		footer << %Q|<div style="position:absolute;top:0px;left:0px;color:#000;background-color:#fff;padding:32px;">この日記は更新が止まっているので変更不可になっています。再度更新したい場合には<a href="https://www.tdiary.net/bbs/support.html">掲示板<a>にてその旨をお知らせ下さい。折返し管理者より連絡します。</div>|
 	end
 
 	footer
@@ -13,7 +13,7 @@ end
 
 def clear_tdiarynet_cache(date)
 	begin
-		url = URI("http://proxy.tdiary.net/cache/#{@conf.user_name}")
+		url = URI("https://proxy.tdiary.net/cache/#{@conf.user_name}")
 		puts "I, request to clear URL: #{url}"
 		params = {proxy_key: ENV['PROXY_KEY']}
 		if date
